f997a25e060b10c63c81b3d1fe10775cf13af804
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-21 18:22:33 +0900

    サインアップ時にname項目の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,8 @@ class ApplicationController < ActionController::Base
   # 未ログイン時にログインページへ遷移する設定
   # 設定したいコントローラーに記述（今回は全てのページに適用させる為、ここ）
   before_action :authenticate_user!
-
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-  # 『devise_parameter_sanitizer』メソッドはDeviseのコントローラ以外で使用不可。
-  # （deviseで追加されたメソッドの為）
-  # すべてのコントローラがApplicationControllerを継承している為、
-  # すべてのコントローラのアクションの前で『devise_parameter_sanitizer』メソッドが呼び出されてしまう。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # 下と1セット
 
   # # サインアウト後のリダイレクト先を設定
   # # 引数には『resource』を渡す（deviseのメソッドを上書きしている為）
@@ -19,8 +15,9 @@ class ApplicationController < ActionController::Base
   # end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   # devise_parameter_sanitizer.permit(追加したいメソッドの種類, keys: [追加したいパラメーター名])
+  # 上と1セット
 
   end
 
