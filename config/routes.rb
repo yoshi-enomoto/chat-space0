e5a82ff20e1a901a84e64c2d1f782677eb07b223
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-21 17:00:09 +0900

    gem deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'messages#index'  #『root』を設定する場合は『コントローラー#アクション』
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
