7278e46cee1fa22610c9f585b0bf469623b34066
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-21 13:53:59 +0900

    rootの設定、および使用しない物の生成を止める記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'messages#index'  #『root』を設定する場合は『コントローラー#アクション』
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
