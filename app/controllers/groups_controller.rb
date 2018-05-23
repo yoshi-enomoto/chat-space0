68b25d07fb47a60f4c80b9fdb07e274b2414e2fd
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-23 15:37:10 +0900

    バリデートでのエラーメッセージ表示

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,11 +4,16 @@ class GroupsController < ApplicationController
   end
 
   def create
-    # @group = Group.new(group_params)
-    # @group.save
-    # 上記２行は下記１行で賄える。
-    Group.create(group_params)
-    redirect_to root_path, notice: "グループ作成完了"
+    # Group.create(group_params)
+    # 下記aとbは上記１行で賄える。
+    @group = Group.new(group_params) #『a』
+    # 『@group.user_ids』でグループ作成時に加えたuser_idが取得可能。
+
+    if @group.save #『b』
+      redirect_to root_path, notice: "グループ作成完了"
+    else
+      render :new
+    end
     # 流れ：params内のpermitのみをDBへ保存 → redirectさせる、かつflashを『notice』キーで表示させる。
     # グループ作成の過程中、『notice、alert』以外のキー指定では表示不可（原因不明）
   end
