04c3494ab14117fbb502ba6f0703fc156f6b46ed
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-24 19:18:44 +0900

    テキストの送信のみ、可能な状態

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,22 @@
 class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @group = Group.find(params[:group_id])
+
+    if @message.save
+      redirect_to group_messages_path(@group), notice: "メッセージ送信完了"
+    else
+      render :index, alert: "メッセージ送信失敗"
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
