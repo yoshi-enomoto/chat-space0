045108961c5cf577c79673e9f0bd1de731d6f7bd
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-24 16:12:26 +0900

    DBと連動したグループ作成（作成時にユーザーも加える）

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,16 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @group.users << current_user
+    # 『@group.users』は配列になる。
+      # @user = User.all
+      # @user =[] << User.find(2)
+      # 『collection_check_boxes』の第2引数（リスト一覧）は配列を展開表示しているみたいなので、『[] << ~~ 』で個別指定も可能。
   end
 
   def create
     # Group.create(group_params)
-    # 下記aとbは上記１行で賄える。
+    # 下記aとbは、上記１行で賄える。
     @group = Group.new(group_params) #『a』
     # 『@group.user_ids』でグループ作成時に加えたuser_idが取得可能。
 
@@ -26,6 +31,11 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name)
+    # params.require(:group).permit(:name)：中間テーブルに保存させる前のparamsの仕様
+    params.require(:group).permit(:name, user_ids: [])
+    # 中間テーブルへの保存は『user_ids: []』の記述を追加する。（配列のパラメータ）
+      # 今回、memberテーブルへレコード保存時、『group_id』が入力出来ないエラーが発生。
+      # →テーブルの仕様『null:false』を削除することで回避。
+      # 旧chat-spaceではその仕様を削除せずにmemberテーブルへレコード保存出来たので、今回この様になってしまった理由が気になるところ。
   end
 end
