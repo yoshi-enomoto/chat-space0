77c798e5eca11b753cff6d727e65f77b40a984d3
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-24 18:05:22 +0900

    使用ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'  #『root』を設定する場合は『コントローラー#アクション』
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+    # resources :messages, only: [:new, :create]
+    # 『index』でメッセージ入力も出来るので上1行は間違い（newだと、一覧表示は違う）
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
