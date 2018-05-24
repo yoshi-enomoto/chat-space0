f30009b8d6a6b77f2be8b96c79c351bb0aa0e340
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-24 15:30:18 +0900

    ※テーブルの仕様変更※
    →グループ作成時、中間テーブルに『group_id』が入力できないエラーが発生。
    →解決方法として、中間テーブルのカラムに定義していた、『null:false』を削除。
    
    →DBへのレコード保存・挿入の順番によるものと思われるが、旧chat-spaceでは『null:false』を設定していても保存できた為、少々気
    になるところ。

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 20180522085656) do
   end
 
   create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",    null: false
-    t.integer  "group_id",   null: false
+    t.integer  "user_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_members_on_group_id", using: :btree
