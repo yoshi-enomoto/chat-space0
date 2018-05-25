ee06e804b853c7452171b1834e8284420f2f2762
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-25 12:52:34 +0900

    バリデートによるエラー発動時の流れを設定
    ・『flash.now』の表示
    ↓
    ・インスタンス変数の格納
    ↓
    ・『render』による表示させたいviewの指定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,11 +13,30 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    # @message = @group.messages.new(message_params) 模範解答での記述
+    # 『@group.message』ではエラーとなる。
+    # 『@group.messages』で該当グループに関連するメッセージが取得可能。
+    # 『~es.new』とすることで、単体用のインスタンスが生成可能。
 
     if @message.save
       redirect_to group_messages_path(@group), notice: "メッセージ送信完了"
     else
-      render :index, alert: "メッセージ送信失敗"
+      # 『flash』は記述の順番が重要（後に記述すると表示されない）
+      flash.now[:alert] = "メッセージを入力してください。"
+      # 『.now』は『render』の際に使用。
+      # 現在のリクエストのみ有効。次のリクエストになると削除される。
+        # flash[:alert] = "メッセージは入力しましたか？"
+        # 『.now』無しは『redirect_to』の際に使用。
+        # 次のリクエストまで有効。その次のリクエストになると削除される。
+        #    無しを試す場合、『flash』が表示されないリクエスト（グループ編集orユーザー編集）
+        #    などに遷移してみれば、意味がわかる。
+
+      @messages = @group.messages.includes(:user)
+      render :index
+      # 『render』は、ビューの描写のみ。
+      # （その該当アクションは通らないので、インスタンス変数などを参照しない）
+      # なので、『render』前に『@messages』を記述する。
+      # その記述順序が逆だと参照されない。
     end
   end
 
