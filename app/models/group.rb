3a265c394277a6db50c4d725386a2be0989130ee
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-25 13:41:41 +0900

    ヘルパーメソッド定義の修正（group.rb）
    ・複数行のコメアウトの記述修正
    ・適用させたいヘルパー定義の内容修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,23 +7,26 @@ class Group < ApplicationRecord
 
   def show_last_message
     if (last_message = messages.last).present?
-      last_message.content? ? last_message.content : '画像が投稿されています'
+      last_message.body? ? last_message.body : '画像が投稿されています'
       # 条件式 ? trueの時の値 : falseの時の値 （三項演算子の書き方）
     else
       'まだメッセージはありません。'
     end
   end
 
-  # 三項演算子は下記の様に書き換え可能。
-    def show_last_message
-      if (last_message = messages.last).present?
-        if last_message.body? # 『.body?』とすることで『.present?』を省いて書ける。
-          last_message.body
-        else
-          "画像が投稿されています"
-        end
+=begin
+  ・三項演算子は下記の様に書き換え可能。
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.body? # 『.body?』とすることで『.present?』を省いて書ける。
+        last_message.body
       else
-        "まだメッセはないよん♪"
+        "画像が投稿されています"
       end
+    else
+      "まだメッセはないよん♪"
     end
+  end
+=end
 end
