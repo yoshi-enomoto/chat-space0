0039ca6fc8bd2561457516a66f4e23645f65da80
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-30 19:06:33 +0900

    createアクションのテストの追記
    ◾︎ログインしているかつ、保存に成功した場合
    ・メッセージの保存はできたのか
    ・意図した画面に遷移しているか
    ◾︎ログインしているが、保存に失敗した場合
    ・メッセージの保存は行われなかったか
    ・意図したビューが描画されているか
    ◾︎ログインしていない場合
    ・意図した画面にリダイレクトできているか

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,23 +1,34 @@
 require 'rails_helper'
+  # describe 'HTTPメソッド名 #アクション名' do
+  #   it "インスタンス変数は期待した値になるか？" do
+  #     "擬似的にリクエストを行ったことにするコードを書く"
+  #     "エクスペクテーションを書く"
+  #   end
 
-# 『メッセージ一覧ページを表示するアクション』での確認事項
-# ●ログインしている場合
-#  ・アクション内で定義しているインスタンス変数があるか
-#    ->@message、@group
-#  ・該当するビューが描画されているか
-#    ->#index
-# ●ログインしていない場合
-#  ・意図したビューにリダイレクトできているか
-#    ->#indexからのリダイレクト
+  #   it "期待するビューに遷移するか？" do
+  #     "擬似的にリクエストを行ったことにするコードを書く"
+  #     "エクスペクテーションを書く"
+  #   end
+  # end
 
 describe MessagesController do
-  describe '#index' do
-    # letを利用してテスト中使用するインスタンスを定義
-    # FactoryGirlで作成
-    let(:group) { create(:group) }
-    let(:user) { create(:user) }
+  # letを利用してテスト中使用するインスタンスを定義
+  # テスト中＆『index』と『create』で適用させたいので、ここに書く。
+  # FactoryGirlで作成
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
 
-    # ログインしている場合
+  # 『メッセージ一覧ページを表示するアクション』での確認事項
+  # ●ログインしている場合
+  #  ・アクション内で定義しているインスタンス変数があるか
+  #    ->@message、@group
+  #  ・該当するビューが描画されているか
+  #    ->#index
+  # ●ログインしていない場合
+  #  ・意図したビューにリダイレクトできているか
+  #    ->#indexからのリダイレクト
+  describe '#index' do
+    # ●ログインしている場合
     context 'log in' do
       before do
         # 作ったユーザでログイン
@@ -46,7 +57,7 @@ describe MessagesController do
       end
     end
 
-    # ログインしていない場合
+    # ●ログインしていない場合
     context 'not log in' do
       before do
         # 『擬似的に動かすアクションのリクエストを共通処理化』
@@ -61,19 +72,80 @@ describe MessagesController do
 
   end
 
+  # 『メッセージ作成するアクション』での確認事項
+  # ●ログインしているかつ、保存に成功した場合
+  #  ・メッセージの保存はできたのか
+  #  ・意図した画面に遷移しているか
+  # ●ログインしているが、保存に失敗した場合
+  #  ・メッセージの保存は行われなかったか
+  #  ・意図したビューが描画されているか
+  # ●ログインしていない場合
+  #  ・意図した画面にリダイレクトできているか
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+    # 『create』を擬似的に動かす際に引数として渡す為、『params』を定義
+    # 『attirbutes_for』：オブジェクトを生成せずにハッシュを生成する。
 
+    # ●ログインしている、
+    context "log in" do
+      before do
+        login_admin user
+        # 『#index』テストとは異なり、擬似的にアクションを動かす記述は不要。
+        # ここに『index』が無くて問題無い。
+      end
 
+      # ●かつ、保存に成功した場合
+      context "can save" do
+        # expectの引数の切り出し
+        subject {
+          post :create,
+          params: params
+        }
 
-  # describe 'HTTPメソッド名 #アクション名' do
-  #   it "インスタンス変数は期待した値になるか？" do
-  #   "擬似的にリクエストを行ったことにするコードを書く"
-  #     "エクスペクテーションを書く"
-  #   end
+        it "count up message" do
+          expect{ subject }.to change(Message, :count).by(1)
+          #「postメソッドでcreateアクションを擬似的にリクエストをした結果」の意味
+          # マッチャに『期待値』：レコードの総数が1個増えたかを確かめる。
+        end
 
-  #   it "期待するビューに遷移するか？" do
-  #     "擬似的にリクエストを行ったことにするコードを書く"
-  #     "エクスペクテーションを書く"
-  #   end
-  # end
+        it "redirects to group_messages_path" do
+          subject
+          # 『post :create』の記述で擬似的に動かすアクションのリクエストとなる。
+          # 同時に、『params: params』が渡すパラメータ（引数）部分である。
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      # ●かつ、保存に失敗した場合
+      context "can't save" do
+        # エラーが起こるparamsの作成
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, body: nil, image: nil) } }
+
+        # 上記で設定したparamsを渡す
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it "doesn't count up" do
+          expect{ subject }.not_to change(Message, :count)
+          # 『.not_to』は『.to』の真逆（〜でないこと。の意）
+        end
+
+        it "renders the :index template" do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    # ●ログインしていない場合
+    context "not log in" do
+      it "redirect_to new_user_session_path" do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 
 end
