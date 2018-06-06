ebb34b1c33148737fb24eb2c1a344ee16384c51d
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-07 01:19:48 +0900

    差し込むhtmlの詳細内容記述

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,6 +2,21 @@ $(function() {
   // htmlを差し込む要素名を変数定義
   var search_user = $("#user-search-result");
 
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>`
+    search_user.append(html);
+  }
+
+  function appendNoUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user}</p>
+                </div>`
+    search_user.append(html);
+  }
+
   // jqオブジェクトを変数定義
   var $input = $("#group_user-search-field");
 
