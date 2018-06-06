03a6cdc24bf3584a07f842fc532b2db7f795669c
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-06 18:49:17 +0900

    通信成功時、失敗時の処理分け

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(function(){
+
   // 下記は指定した要素のみを取得
   // $(".form__submit").on("click", function(e) {
   // 下記は指定したform要素、中全てを取得=thisの利便性向上（ajax通信時に楽）
@@ -16,5 +17,13 @@ $(function(){
       contentType: false
     })
 
+    .done(function(data){
+
+    })
+
+    .fail(function(){
+      alert("通信に失敗しました")
+
+    })
   });
 });
