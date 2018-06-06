bded59067e61d5b025f18849755b05ec0d0243ff
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-07 01:16:58 +0900

    Ajax通信の送信内容

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,6 +5,15 @@ $(function() {
   // 本処理ー『チャットメンバーを追加』エリアにユーザーの表示
   $input.on("keyup",function(){
     var input = $input.val();
+
+    $.ajax({
+      type: "get",
+      // 『url』には入力内容の保存先ではなく、検索をする元になるインスタンス変数があるアクションを動かすurlを指定する。
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+
   });
 
 });
